7aac2a5b8a7a384e46a235e288a44bde89bda3f5
CosmoBlack <soulker_mozyr@yahoo.com>
2023-02-28 21:18:53 +0200

    Initial commit

---
+++ src/main/java/pro/sky/cosmoblackcalculator/CosmoBlackCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cosmoblackcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoBlackCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoBlackCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/cosmoblackcalculator/CosmoBlackCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cosmoblackcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoBlackCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
